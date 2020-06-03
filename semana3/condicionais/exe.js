c461f03abd69211fa74994ae11e0a28e352c9ff2
rcarlos12 <48447957+rcarlos-silva@users.noreply.github.com>
2020-06-03 16:56:12 -0300

    Exercicio incompleto

---
+++ semana3/condicionais/exe.js
@@ -0,0 +1,3 @@
+EXERCÍCIO 1
+Explique o que o código faz. Qual o teste que ele realiza? Para que tipos de números ele imprime no console "Passou no teste"? Para que tipos, a mensagem é "Não passou no teste"? 
+Passou no teste numeros pares e Não passou para numeros impares!
