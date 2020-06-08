b7edafb0d1be9bf4a8bc322e630e5f3ffbec200b
rcarlos12 <48447957+rcarlos-silva@users.noreply.github.com>
2020-06-07 22:33:46 -0300

    So estou com problemas para fazer os testes e sair depois

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -10,4 +10,56 @@
  * 
  * 
  */
+let Usuario = [];
+let Computador = [];
+let UsTxt = []
+let CpTxt = []
+let somaUs = 0
+let somaCp = 0
+console.log('Bem vindo ao jogo de Blackjack!');
+if (confirm('Deseja iniciar uma nova rodada?')) {
+   for (i = 0; i < 2; i++) {
+      Usuario[i] = comprarCarta()
+      UsTxt += Usuario[i].texto
+      somaUs += Usuario[i].valor
+   }
+   for (a = 0; a < 1; a++) {
+      Computador[a] = comprarCarta()
+      somaCp += Computador[a].valor
+      CpTxt += Computador[a].texto
+   }
+   while (somaUs < 21) {
+      if (confirm(`Suas cartas são ${UsTxt}. A carta revelada do Computador é ${Computador[0].texto}.\n Deseja comprar mais uma carta? `)) {
+         Usuario[Usuario.length - 1] = comprarCarta()
+         UsTxt += Usuario[Usuario.length - 1].texto
+         somaUs += Usuario[Usuario.length - 1].valor
+         while (somaCp < 21) {
+            Computador[a] = comprarCarta()
+            somaCp += Computador[a].valor
+            CpTxt += Computador[a].texto
+         }
+      } else {
+         while (somaCp < 21) {
+            Computador[a] = comprarCarta()
+            somaCp += Computador[a].valor
+            CpTxt += Computador[a].texto
+         }
+         if (somaUs > 21 || somaCp === 21 || (21 - somaUs) < (21 - somaCp)) {
+            alert(`Sua pontuação foi ${somaUs} e a do computador foi ${somaCp}. \n O Computador Ganhou!`)         
+         } else if (somaCp > 21 || somaUs === 21 || (21 - somaUs) > (21 - somaCp)) {
+            alert(`Sua pontuação foi ${somaUs} e a do computador foi ${somaCp}. \n O Usuario Ganhou!`)        
+         }else
+         console.log(21 - somaUs)
+         console.log(21 - somaCp);
+      }
+      if (somaUs > 21 || somaCp === 21 || (21 - somaUs) < (21 - somaCp)) {
+         alert(`Sua pontuação foi ${somaUs} e a do computador foi ${somaCp}. \n O Computador Ganhou!`)         
+      } else if (somaCp > 21 || somaUs === 21 || (21 - somaUs) > (21 - somaCp)) {
+         alert(`Sua pontuação foi ${somaUs} e a do computador foi ${somaCp}. \n O Usuario Ganhou!`)        
+      }
+   }
+   
 
+} else {
+   console.log("O jogo Acabou!");
+}
