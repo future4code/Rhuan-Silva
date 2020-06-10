16816fc88702912a6b9f92bd8eaadc0f987b689b
rcarlos12 <48447957+rcarlos-silva@users.noreply.github.com>
2020-06-10 16:38:32 -0300

    Exe incompleto

---
+++ semana4/objetos/blog/script.js
@@ -0,0 +1,12 @@
+let meuArray = []
+function postar() {
+    const post = {
+        autor: document.form.querySelector('#autor'),
+        titulo: document.form.querySelector('#titulo'),
+        conteudo: document.form.querySelector('#conteudo')
+    }
+    meuArray.push() = post
+    console.log(meuArray);
+}
+
+
