46ef60e4da3b87cb8bef5e2b634a8833159a11f6
rcarlos12 <48447957+rcarlos-silva@users.noreply.github.com>
2020-06-12 11:02:56 -0300

    Exercicio 2

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -3,4 +3,7 @@ Exercícios de interpretação de código
 // 1. A função recebe o valor em dolar de entrada, o valor da cotação do dia e multiplica o valor de entrada pela cotação, e retorna o valor em reais na variavel meuDinheiro
 // o console vai imprimir : R$ 100*contacao
 
-2.
+// 2. A função recebe um valor de entrada e um tipo de investimento, testa qual tipo de investivemento foi escolhido e retorna o valor apos o investimento. 
+// o console vai imprimir na primeira linha 165
+// o console vai imprimir na segunda linha nada, vai informar 'TIPO DE INVESTIMENTO INFORMADO INCORRETO!'
+
