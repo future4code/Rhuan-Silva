f0ee10a54c28d3d87584d41618fa5e1145df2b3f
rcarlos12 <48447957+rcarlos-silva@users.noreply.github.com>
2020-06-12 11:38:55 -0300

    Exercicio logica 3

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -1,35 +1,43 @@
-Exercícios de interpretação de código
+// Exercícios de interpretação de código
 
-// 1. A função recebe o valor em dolar de entrada, o valor da cotação do dia e multiplica o valor de entrada pela cotação, e retorna o valor em reais na variavel meuDinheiro
-// o console vai imprimir : R$ 100*contacao
+// // 1. A função recebe o valor em dolar de entrada, o valor da cotação do dia e multiplica o valor de entrada pela cotação, e retorna o valor em reais na variavel meuDinheiro
+// // o console vai imprimir : R$ 100*contacao
 
-// 2. A função recebe um valor de entrada e um tipo de investimento, testa qual tipo de investivemento foi escolhido e retorna o valor apos o investimento. 
-// o console vai imprimir na primeira linha 165
-// o console vai imprimir na segunda linha nada, vai informar 'TIPO DE INVESTIMENTO INFORMADO INCORRETO!'
+// // 2. A função recebe um valor de entrada e um tipo de investimento, testa qual tipo de investivemento foi escolhido e retorna o valor apos o investimento. 
+// // o console vai imprimir na primeira linha 165
+// // o console vai imprimir na segunda linha nada, vai informar 'TIPO DE INVESTIMENTO INFORMADO INCORRETO!'
 
-// 3. Esta testando quais numeros do array numeros é par e jogando para o array1 e os impares joga para o array2
-// Depois vai exibir no console o tamanho do array inicial e dos outros 2 novos arrays.
+// // 3. Esta testando quais numeros do array numeros é par e jogando para o array1 e os impares joga para o array2
+// // Depois vai exibir no console o tamanho do array inicial e dos outros 2 novos arrays.
 
-// o console vai imprimir na primeira linha 14
-// o console vai imprimir na segunda linha 6
-// o console vai imprimir na terceira linha 8
+// // o console vai imprimir na primeira linha 14
+// // o console vai imprimir na segunda linha 6
+// // o console vai imprimir na terceira linha 8
 
-// 4. O código está testando qual é o menor e qual o maior numero do array e salvando dentro das variaveis respectivamente. 
+// // 4. O código está testando qual é o menor e qual o maior numero do array e salvando dentro das variaveis respectivamente. 
 
-// o console vai imprimir na primeira linha -10
-// o console vai imprimir na segunda linha 1590
+// // o console vai imprimir na primeira linha -10
+// // o console vai imprimir na segunda linha 1590
 
-Exercícios de Lógica de Programação
+// Exercícios de Lógica de Programação
 
-// 1. while, for(inicio;condicao;atribuicao), for(let indice of array)
-// for (i = 0; i <= 10; i++){
-//     console.log(i);    
+// // 1. while, for(inicio;condicao;atribuicao), for(let indice of array)
+// // for (i = 0; i <= 10; i++){
+// //     console.log(i);    
+// // }
+
+// // 2. 
+// // a) false
+// // b) true 
+// // c) true
+// // d) true
+// // e) false  
+
+// 3. O codigo não funciona pois está incompleto.
+
+// const quantidadeDeNumerosPares = 5
+// for(i = 0;i < quantidadeDeNumerosPares*2; i++) {
+//     if(i%2===0){
+//         console.log(i)
+//     }
 // }
-
-// 2. 
-// a) false
-// b) true 
-// c) true
-// d) true
-// e) false  
-
