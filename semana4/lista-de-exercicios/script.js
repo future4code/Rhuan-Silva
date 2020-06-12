be091d86f06ccb76ff632575ff8c84cd64c93d7a
rcarlos12 <48447957+rcarlos-silva@users.noreply.github.com>
2020-06-12 10:53:54 -0300

    Exercicio 1

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,6 @@
+Exercícios de interpretação de código
+
+// 1. A função recebe o valor em dolar de entrada, o valor da cotação do dia e multiplica o valor de entrada pela cotação, e retorna o valor em reais na variavel meuDinheiro
+// o console vai imprimir : R$ 100*contacao
+
+2.
